2c2db46fa8f82ae1030ff3a6cba2769375bc41be
Rudenok <rudenokart@yandex.ru>
2022-05-04 18:06:32 +0300

    task_1549 - save

--- php_interface/include/classes/Highload_Block.php
+++ php_interface/include/classes/Highload_Block.php
@@ -8,14 +8,38 @@ class Highload_Block extends InfoBlock {
     // $this->ts_services_list = $this->getTsServicesList();
   }
 
-  function getBookingsForService ($service_id, $dete) {
+  function getPricesForService ($service_id, $unix_date) {
+    $highloadblock = $this->getHighloadBlock([
+      'filter'=>['TABLE_NAME' => 'ts_prices',],
+    ]);
+    $entity = Bitrix\Highloadblock\HighloadBlockTable::compileEntity($highloadblock);
+    $entity_data_class = $entity->getDataClass();
+    $rsData = $entity_data_class::getList([
+      'select' => ['ID', 'UF_GROSS', 'UF_PTPR_ID', 'UF_SERVICE_ID', 'UF_DATE'],
+      'filter' => [
+        'UF_SERVICE_ID'=>$service_id,
+        'UF_DATE' => $unix_date,
+      ],
+    ]);
+    $arr = [];
+    $tooday = strtotime(date("Y-m-d 0:0:0"));
+    foreach ($rsData as $key => $value) {
+      if ($tooday <= $value['UF_DATE']) {
+        $date = getdate($value['UF_DATE']);
+        $value['date'] = $date['mday'].'.'.$date['mon'].'.'.$date['year'];
+        array_push($arr, $value);
+      }
+    }
+    return $arr;
+  }
+
+  function getBookingsForService ($service_id, $date) { // заказы (бронирования)
     $highloadblock = $this->getHighloadBlock([
       'filter'=>['TABLE_NAME' => 'bookings',],
     ]);
     $entity = Bitrix\Highloadblock\HighloadBlockTable::compileEntity($highloadblock);
     $entity_data_class = $entity->getDataClass();
     $rsData = $entity_data_class::getList([
-      'filter' => ['UF_IBLOCK_ELEMENT_ID'=>2322],
       'select' => ['ID','UF_SERVICE_JSON'],
     ]);
     $arr = [];
@@ -25,14 +49,19 @@ class Highload_Block extends InfoBlock {
       $value['dateBegin'] = $value['UF_SERVICE_JSON']['dateBegin'];
       $value['dateEnd'] = $value['UF_SERVICE_JSON']['dateEnd'];;
       if (
-        $service_id == $value['UF_SERVICE_ID']) {
+        strtotime($date) >= strtotime($value['dateBegin'])
+        and
+        strtotime($date) <= strtotime($value['dateEnd'])
+        and
+        $service_id == $value['UF_SERVICE_ID']
+      ) {
         array_push($arr, $value);
       }
     }
     return $arr;
   }
 
-  function getTsQuotasForService ($service_id) {
+  function getTsQuotasForService ($service_id) { // Получить квоты, бронирования, свободные номера
     $highloadblock = $this->getHighloadBlock([
       'filter'=>['TABLE_NAME' => 'ts_quotas',],
     ]);
@@ -41,9 +70,13 @@ class Highload_Block extends InfoBlock {
     $entity_data_class = $entity->getDataClass();
     $rsData = $entity_data_class::getList(['filter' => ['UF_SERVICE_ID'=>$service_id]]);
     $arr = [];
+    $tooday = strtotime(date("Y-m-d 0:0:0"));
     foreach ($rsData as $key => $value) {
-      $value['date'] = ConvertTimeStamp($value['UF_DATE']);
-      array_push($arr, $value);
+      if ($tooday <= $value['UF_DATE']) {
+        $value['date'] = ConvertTimeStamp($value['UF_DATE']);
+        $value['available_numbers'] = $value['UF_QUOTE'] - $value['UF_SOLD_NUMBER'];
+        array_push($arr, $value);
+      }
     }
     return $arr;
   }
