3a389185f756f70231e948384e04e3b37a817f52
Rudenok <rudenokart@yandex.ru>
2022-05-10 11:13:37 +0300

    task_1583 - source_id add

--- php_interface/include/classes/B24_Leads.php
+++ php_interface/include/classes/B24_Leads.php
@@ -8,7 +8,15 @@ class B24_Leads extends B24_Partners {
   //   $this->request_form_data = $this->getRequestFormData();
   // }
 
-  function leadAdd ($method, $query) { 
+  function getLeadList ($filter=[]) {
+    $arr = $this->restApiRequest('crm.lead.list', [
+      'filter' => $filter,
+      'select' => ['*', 'UF_*'],
+    ]);
+    return $arr;
+  }
+
+  function restApiRequest ($method, $query) { 
     $api_method = $method.'?'; 
     $api_query = http_build_query($query); 
     $result = file_get_contents(self::WEB_HOOK.$api_method.$api_query); 
@@ -16,14 +24,15 @@ class B24_Leads extends B24_Partners {
   }
 
   function getRequestFormData () { // получение данных и  создание лида для формы заявки на экскурсию
-    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/test.json', json_encode($_POST));
-    $this->leadAdd('crm.lead.add', [
+    // file_put_contents($_SERVER['DOCUMENT_ROOT'].'/test.json', json_encode($_POST));
+    $this->restApiRequest('crm.lead.add', [
       'fields' =>[
         'TITLE' => 'Заявка на экскурсию: '.$_POST['object_name'],
         'NAME' => $_POST['full_name'],
         'STATUS_ID' => 'NEW',
         'OPENED' => 'Y',
         'ASSIGNED_BY_ID' => 287,
+        'SOURCE_ID' => 10,
         'COMMENTS'=> $_POST['comment'],
         'PHONE' => [['VALUE'=> $_POST['phone'], 'VALUE_TYPE'=>'WORK']],
         'EMAIL' => [['VALUE'=> $_POST['email'], 'VALUE_TYPE'=>'WORK'],],
