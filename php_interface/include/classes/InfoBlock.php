b137152970bd7755d256378aa05747f5f69f6068
Rudenok <rudenokart@yandex.ru>
2022-05-12 18:06:05 +0300

    Citys list for samo tour

--- php_interface/include/classes/InfoBlock.php
+++ php_interface/include/classes/InfoBlock.php
@@ -4,6 +4,21 @@
  */
 class InfoBlock {
 
+  function getCitysListForSamoTour () { // получить список городов (областей)
+    return $this->getSectionsList([],['SECTION_ID'=>57], false, ['ID', 'NAME', 'CODE']);
+  }
+
+  function getHotelsListForSamoTour () {
+    return $this->getItemsList([],[
+      'SECTION_ID'=>68,
+    ]);
+  }
+
+  function getSectionsList ($order=[], $filter=[], $quantity=false, $select=[], $nav=false) {
+    $res = CIBlockSection::GetList($order, $filter, $quantity, $select, $nav);
+    return $this->getList_fetch($res);
+  }
+
   function getItemsList ($order=[], $filter=[], $group=false, $nav=false, $select=[]) {
     $res = CIBlockElement::GetList($order, $filter, $group, $nav, $select);
     return $this->getList_fetch($res);
