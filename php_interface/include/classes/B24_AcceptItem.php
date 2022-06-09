75347a10b7119af3884f8875eb550140f3d67ac9
Rudenok <rudenokart@yandex.ru>
2022-06-09 09:27:27 +0300

    addTaskForActivateObj

---
+++ php_interface/include/classes/B24_AcceptItem.php
@@ -0,0 +1,58 @@
+<?php 
+
+
+//При запросе на активацию объекта ставится задача в битрикс24 на подразделение "Работа с кабинетами поставщиков (Техническое подразделение)"
+class B24_AcceptItem extends B24_class {
+
+        private $elementID;
+
+        function __construct($elementID){
+              $this->elementID = $elementID;
+          }
+
+        function acceptItem () {
+          $users = $this->RestApiRequest('user.search', [
+            //ID подразделения
+            'UF_DEPARTMENT' => 57,
+        ]);
+
+        $accomplices = [];
+
+          // if(count($users['result']) >= 1){
+          //   $users = json_decode($users, true);
+          //   $creatorID = $users['result'][0]['ID'];
+          //   foreach($users['result'] as $user){
+          //     $accomplices[] = $user['ID'];
+          //   }
+          // }else{
+          //   $creatorID = 25299;
+          //   $accomplices[] = 25299;
+          // }
+
+
+
+
+          $users = json_decode($users, true);
+          $creatorID = $users['result'][0]['ID'];
+          foreach($users['result'] as $user){
+            $accomplices[] = $user['ID'];
+          }
+          
+          $desc = "Активировать объект: id $this->elementID";
+
+           $this->RestApiRequest('tasks.task.add', [
+             'fields'=>[
+               'TITLE'=>'Активация объекта',
+               'CREATED_BY' => $creatorID,
+               'RESPONSIBLE_ID' => $creatorID,
+               'ACCOMPLICES' => $accomplices,
+               'DESCRIPTION' => $desc,
+             ]
+           ]);
+        }
+          
+    }
+    
+    ?>
+
+    
