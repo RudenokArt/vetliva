6220f80b3ab12ab7c5b68e013290cf25f1377dd0
root <root@192.168.230.101>
2022-06-30 15:50:33 +0300

    thanks_page

---
+++ components/vetliva/thanks_page/templates/.default/lang/en/template.php
@@ -0,0 +1,1 @@
+<?php $MESS['THANKS'] = 'Thank you! Your application is accepted. We will contact you shortly.';?>
---
+++ components/vetliva/thanks_page/templates/.default/template.php
@@ -0,0 +1,11 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+      echo '<div class="container thanks">
+      <div class="thanks-content">
+        <h3>Спасибо! Ваша заявка принята. В ближайшее время мы свяжемся с Вами. </h3>
+      </div>
+      <div class="back">
+     <a href="../" class="awe-btn awe-btn-1 awe-btn-small">Вернуться на главную</a>
+      </div>
+    </div>
+     <br>';
+?>
